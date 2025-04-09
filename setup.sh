e29a060f4983849050391f5f406c95e70e159c86
Will Stuckey <william.stuckey@trane.com>
2025-04-09 16:20:27 -0500

    initial

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install Zsh and Git
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  sudo apt update && sudo apt install -y zsh git
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  brew install zsh git
+fi
+
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# Clone dotfiles and symlink .zshrc
+git clone https://github.com/wstuckey/dotfiles.git ~/dotfiles
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+
+# Change shell to Zsh (if not already)
+chsh -s $(which zsh)
+
+echo "Done! Restart your terminal."
