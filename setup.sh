d4b488a608e26087ea2e9a106bebb5ece4fb6e99
Will Stuckey <18338408+wstuckey@users.noreply.github.com>
2025-04-14 17:15:53 -0500

    Update setup.sh
    add rust install

--- setup.sh
+++ setup.sh
@@ -10,6 +10,9 @@ fi
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
+# Install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -y
+
 # Clone dotfiles and symlink .zshrc
 git clone https://github.com/wstuckey/dotfiles.git ~/dotfiles
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
